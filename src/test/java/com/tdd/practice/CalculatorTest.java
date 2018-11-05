61afd39053d6e226285e6f46c2c166d793e017c3
Shan Sankara Pandian Muthu <shan.sankara-pandian-muthu@capgemini.com>
2018-11-05 21:15:54 +0000

    tdd implementations

--- src/test/java/com/tdd/practice/CalculatorTest.java
+++ src/test/java/com/tdd/practice/CalculatorTest.java
@@ -1,6 +1,8 @@
 package com.tdd.practice;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -13,6 +15,22 @@ public class CalculatorTest {
 
     @Before
     public void setUp(){
-
+        calculator = new Calculator();
     }
+
+    @Test
+    public void givenStringNosPresentTest() throws Exception{
+        Assert.assertNotNull("Given Strings No's are available to do calculation", givenStringNos);
+    }
+
+    @Test
+    public void givenStringsNotEmptyTest() throws Exception{
+        Assert.assertFalse(givenStringNos.length()<0);
+    }
+
+    @Test
+    public void calculatorIsPresent() throws Exception{
+        Assert.assertNotNull(calculator);
+    }
+    
 }
