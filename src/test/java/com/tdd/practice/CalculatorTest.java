ddaddde4a089044e4b3d2f862d64e159ee5f10b6
Shan Sankara Pandian Muthu <shan.sankara-pandian-muthu@capgemini.com>
2018-11-05 21:05:15 +0000

    Calculator introduces RED

--- src/test/java/com/tdd/practice/CalculatorTest.java
+++ src/test/java/com/tdd/practice/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.tdd.practice;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -7,4 +8,11 @@ import org.junit.runners.JUnit4;
 public class CalculatorTest {
 
     private static final String givenStringNos = "1,2,3\n4,100,999,1010, 1005, 4001, 2003";
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp(){
+
+    }
 }
