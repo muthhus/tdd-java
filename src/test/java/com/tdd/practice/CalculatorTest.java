cb25daeaebc5747e0a7c5c25964d1d91ead67a18
Shan Sankara Pandian Muthu <shan.sankara-pandian-muthu@capgemini.com>
2018-11-05 21:04:03 +0000

    Amended given string nos

--- src/test/java/com/tdd/practice/CalculatorTest.java
+++ src/test/java/com/tdd/practice/CalculatorTest.java
@@ -5,4 +5,6 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class CalculatorTest {
+
+    private static final String givenStringNos = "1,2,3\n4,100,999,1010, 1005, 4001, 2003";
 }
