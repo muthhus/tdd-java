33fd4183f022e92ded3bc95c7ead3dbb50e38c15
Shan Sankara Pandian Muthu <shan.sankara-pandian-muthu@capgemini.com>
2018-11-10 08:01:08 +0000

    RED to Green

--- src/main/java/com/tdd/practice/utils/ReverseString.java
+++ src/main/java/com/tdd/practice/utils/ReverseString.java
@@ -1,4 +1,10 @@
 package com.tdd.practice.utils;
 
 public class ReverseString {
+
+    public static final String gnString = "world";
+
+    public String convertString(String input){
+        return null;
+    }
 }
